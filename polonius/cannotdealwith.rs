831878be33804d95348b9ed1ef1a5b6c80cf5d45
lengyijun <sjtu5140809011@gmail.com>
2021-10-27 13:17:02 +0800

    another example which polonius can't handle

--- polonius/cannotdealwith.rs
+++ polonius/cannotdealwith.rs
@@ -1,3 +1,4 @@
+// example 1
 #[derive(Debug)]
 struct S;
 
@@ -12,6 +13,32 @@ fn main() {
     dbg!(v);
 }
 
-// another example 
+// example 2
 // https://github.com/rust-lang/rust/issues/70797
-//
+
+
+// example 3
+// https://rust-lang.zulipchat.com/#narrow/stream/186049-t-compiler.2Fwg-polonius/topic/purely.20forward.20propagation
+fn random_bool() -> bool {
+    unimplemented!()
+}
+
+fn main() {
+    let mut x: (&u32,) = (&0,);
+    let mut y: (&u32,) = (&1,);
+    let mut z = 2;
+
+    if random_bool() {
+        y.0 = x.0; // creates `'x: 'y` subset relation
+    }
+
+    if random_bool() {
+        x.0 = &z; // creates {L0} in 'x constraint
+        // this point, we have `'x: 'y` and `{L0} in `'x`, so we also have `{L0} in 'y`
+        drop(x.0);
+    }
+
+    z += 1; // polonius flags an (unnecessary) error
+
+    drop(y.0);
+}
